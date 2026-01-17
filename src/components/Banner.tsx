c568c75358999758ec4dc28e1719f8bee858f467
CMDP <tu-email@example.com>
2026-01-17 12:30:07 -0600

    Actualizar banner con imagen responsiva

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,13 @@
 'use client'
 
+import Image from 'next/image'
+
 export default function Banner() {
   return (
     <div className="w-full">
-      <img 
+      <Image 
         src="//congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=3840" 
         alt="Congreso Nacional de Dermatología Pediátrica" 
-        srcSet="//congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=375 375w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=550 550w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=750 750w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=1100 1100w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=1500 1500w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=1780 1780w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=2000 2000w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=3000 3000w, //congresodermajal.com.mx/cdn/shop/files/CONGRESO_BANNER_2.jpg?v=1747427278&width=3840 3840w" 
         width={6000} 
         height={2000} 
         sizes="100vw" 
