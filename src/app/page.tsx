aebf31c3b807b2816c723fbd4b59b4df685efc9d
CMDP <tu-email@example.com>
2026-01-18 12:40:12 -0600

    ultimos cambios en los botones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
           </p>
           <a
             href="/cndp"
-            className="hero-btn-primary"
+            className="hero-btn-secondary"
           >
             Registrarse al Congreso
           </a>
